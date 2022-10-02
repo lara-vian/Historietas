09ea001e1825942ee2653d21e0f950a5bc33c735
L4rhy <lara.vsteinbach@gmail.com>
2022-10-02 12:11:57 -0300

    nao sei

--- src/screens/home/style.ts
+++ src/screens/home/style.ts
@@ -2,5 +2,5 @@ import styled from "styled-components/native";
 import {RFPercentage} from "react-native-responsive-fontsize";
 
 export const ListaBotoes = styled.FlatList`
-
+    background-color:${({theme})=>theme.PRIMARIAS.100}
 `;
